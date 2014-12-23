ebe7b0d6a1d4ad7d0dd8ddca1a673a2d9e67d519
Goran Pro <goran.prodanovic@gmail.com>
2014-12-23 18:25:30 +0100

    blabla

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,15 @@
-from fabric.api import local
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
 
 
 def test():
-	local("nosetests -v")
+	with settings(warn_only=True):
+		result=local("python test_tasks.py -v && \
+			python test_users.py -v", capture=True)
+
+	if result.failed and not confirm("Tests failed. Continue?"):
+		abort("Aborted at user request.")
+	
 
 def commit():
 	message = raw_input("enter a git commit message: ")
@@ -10,6 +17,22 @@ def commit():
 
 def push():
 	local("git push origin master")
+
+def pull():
+	local("git pull origin master")
+
+def heroku():
+	local("git push heroku master")
+
+def heroku_test():
+	local("heroku run python test_tasks.py -v && heroku run python test_users.py -v")
+
+def deploy():
+	pull()
+	test()
+	commit()
+	heroku()
+	heroku_test()
 
 def prepare():
 	test()
